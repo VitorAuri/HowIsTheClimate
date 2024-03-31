5a4309ea9d740397b3a304c5fece683640be436a
Vitor <132232270+VitorAuri@users.noreply.github.com>
2024-03-31 00:56:00 -0300

    Ajuste de animação e desativação em telas mobile por causa de bug

--- src/components/CloudAnimation/styles.ts
+++ src/components/CloudAnimation/styles.ts
@@ -1,8 +1,9 @@
 import styled, {keyframes} from "styled-components";
 
+const animationTiming = "linear";
 const cloudAnimation = keyframes`
   0% {
-    left: -18%;
+    left: -60%;
   }
   100% {
     left: 100%;
@@ -17,14 +18,17 @@ left: 0;
 width: 100%;
 height: 100%;
 overflow: hidden;
+@media (max-width: 768px) {
+    display: none; 
+}
 `
 export const FirstCloud = styled.div`
 position: absolute;
 z-index: 0;
 top: 150px;
-left: -18%;
+left: -100%;
 animation-name: ${cloudAnimation};
-animation-timing-function: linear;
+animation-timing-function: ${animationTiming};
 animation-duration: 20s;
 animation-iteration-count: infinite;
 `
@@ -32,9 +36,9 @@ export const SecondCloud = styled.div`
 position: absolute;
 z-index: 0;
 bottom: 0;
-left: -18%;
+left: -100%;
 animation-name: ${cloudAnimation};
-animation-timing-function: linear;
+animation-timing-function: ${animationTiming};
 animation-duration: 19s;
 animation-iteration-count: infinite;
 animation-delay: 10s;
@@ -43,9 +47,9 @@ export const ThirdCloud = styled.div`
 position: absolute;
 z-index: 0;
 top: 0;
-left: -18%;
+left: -100%;
 animation-name: ${cloudAnimation};
-animation-timing-function: linear;
+animation-timing-function: ${animationTiming};
 animation-duration: 17s;
 animation-iteration-count: infinite;
 animation-delay: 8s;
@@ -54,9 +58,9 @@ export const FourthCloud = styled.div`
 position: absolute;
 z-index: 0;
 top: 300px;
-left: -18%;
+left: -100%;
 animation-name: ${cloudAnimation};
-animation-timing-function: linear;
+animation-timing-function: ${animationTiming};
 animation-duration: 22s;
 animation-iteration-count: infinite;
 `
@@ -64,9 +68,9 @@ export const FifthCloud = styled.div`
 position: absolute;
 z-index: 0;
 top: 50px;
-left: -18%;
+left: -100%;
 animation-name: ${cloudAnimation};
-animation-timing-function: linear;
+animation-timing-function: ${animationTiming};
 animation-duration: 19s;
 animation-iteration-count: infinite;
 animation-delay: 6s;
